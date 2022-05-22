9e9506e41fe85c28a1d7c7edd53c9438a1eadfff
michaelt2001 <liangyen02@gmail.com>
2022-05-22 10:34:37 +0700

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,13 @@
+import 'package:dart_packages/hello.dart';
+
+void main() {
+  print(sayHello('Michael'));
+
+  Customer customer = Customer();
+  Category category = Category();
+  Product product = Product();
+
+  print(customer);
+  print(category);
+  print(product);
+}
